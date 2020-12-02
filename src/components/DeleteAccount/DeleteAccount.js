a54d83c53c190c3e8fc325e5fee13b3d4d3fbeb8
shurukian <dvalentine03@gmail.com>
2020-12-02 10:45:59 -0500

    Updated Chat box and Delete Account
    Width changes with the messages size.
    Added a Confirm Delete Modal.
    Updated the delete account message.

--- src/components/DeleteAccount/DeleteAccount.js
+++ src/components/DeleteAccount/DeleteAccount.js
@@ -1,19 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { withRouter } from 'react-router-dom'
 
 import { deleteAccount } from '../../api/auth'
 import messages from '../AutoDismissAlert/messages'
 
 import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+
+const ConfirmDeleteModal = props => {
+  return (
+    <Modal {...props} aria-labelledby="confirm-delete-modal">
+      <Modal.Header closeButton>
+        <Modal.Title id="confirm-delete-modal">
+          Confirm Delete Account
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        Are you sure you want to delete your account?
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant='outline-dark' onClick={props.onHide}>Close</Button>
+        <Button variant='outline-danger' onClick={props.deleteAccount}>Delete Account</Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
 
 const DeleteAccount = props => {
+  const [modalShow, setModalShow] = useState(false)
+
   const onDeleteAccount = event => {
     event.preventDefault()
     const { msgAlert, history, user, clearUser } = props
 
     deleteAccount(user)
       .then(() => msgAlert({
-        heading: 'Delete Account Success',
+        heading: 'Account Successfully Deleted',
         message: messages.deleteAccountSuccess,
         variant: 'success'
       }))
@@ -29,7 +51,9 @@ const DeleteAccount = props => {
   return (
     <div className="row">
       <div className="col-sm-10 col-md-8 mx-auto mt-5">
-        <Button variant="outline-danger" onClick={onDeleteAccount} block>Delete Account</Button>
+        <Button variant="outline-danger" onClick={() => setModalShow(true)} block>Delete Account</Button>
+
+        <ConfirmDeleteModal show={modalShow} onHide={() => setModalShow(false)} deleteAccount={onDeleteAccount} />
       </div>
     </div>
   )
